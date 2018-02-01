28e22649c303848c9b6ea716cdaaf4de59949063
Andrzej Senderecki <33809996+andrzejsenderecki@users.noreply.github.com>
2018-02-01 22:57:48 +0100

    Add files via upload

--- website_cms/articles/models.py
+++ website_cms/articles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=180)
     content = models.TextField()
+    image = models.FileField(upload_to='images/')
     published = models.DateTimeField()
 
     def __str__(self):
--- website_cms/website_cms/urls.py
+++ website_cms/website_cms/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'article/', include('articles.urls'))
-]
+    url(r'article/', include('articles.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
